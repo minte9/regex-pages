15ae948256dbe8d2f47aa6a787f297c10d8565fa
minte9 <catalinprescure@gmail.com>
2022-12-05 17:30:57 +0200

    regex-pages update

--- main/grep/word_boundaries/word_boundaries.sh
+++ main/grep/word_boundaries/word_boundaries.sh
@@ -2,4 +2,13 @@
 
 : "Word boundaries, a common problem:
 A regex that match a word can also match it embedded 
+You can use metasequences \b if is supported by your software
 "
+
+echo ': "result.txt \n' > result.txt
+
+grep 'gray'         sample.txt >> result.txt
+grep '\b(cat)\b' -E sample.txt >> result.txt
+
+cat 'sample.txt'
+cat 'result.txt'
