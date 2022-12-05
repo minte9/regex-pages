4f53dbcc93bf3c63de16bbd867aa9507cdd0c884
minte9 <catalinprescure@gmail.com>
2022-12-05 18:50:56 +0200

    regex-pages update

---
+++ main/grep/optional/optional.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+: "Optional items
+
+Question mark in colou?r means u optional
+It is placed after the character that is optional
+
+The u? part is always successful
+This isn't to say that any regex that contains ? is always successful
+
+For example, in colou?r against 'semicolon', 
+both 'colo' and 'u?' are successful, but r fails, so the whole regex fails
+"
+
+echo ': "result.txt \n' > result.txt
+
+grep 'colou?r' -E sample.txt >> result.txt
+
+cat 'sample.txt'
+cat 'result.txt'
