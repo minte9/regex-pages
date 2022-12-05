c485e5bcb4ecb458a8ff8124a9a172ac01bae236
minte9 <catalinprescure@gmail.com>
2022-12-05 19:02:46 +0200

    regex-pages update

--- main/grep/optional/optional.sh
+++ main/grep/optional/optional.sh
@@ -3,15 +3,7 @@
 : "Optional items
 Question mark in colou?r means u optional
 It is placed after the character that is optional
-
-Grouping for optional, 
-is one of the main uses of parantheses
-
-The u? part is always successful
-This isn't to say that any regex that contains ? is always successful
-
-For example, in colou?r against 'semicolon', 
-both 'colo' and 'u?' are successful, but r fails, so the whole regex fails
+Grouping for optional, is one of the main uses of parantheses
 "
 
 echo ': "result.txt \n' > result.txt
