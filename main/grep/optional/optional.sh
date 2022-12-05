a4771d29837baae765c423d8a80e7c4f42e62791
minte9 <catalinprescure@gmail.com>
2022-12-05 19:02:04 +0200

    regex-pages update

--- main/grep/optional/optional.sh
+++ main/grep/optional/optional.sh
@@ -4,6 +4,9 @@
 Question mark in colou?r means u optional
 It is placed after the character that is optional
 
+Grouping for optional, 
+is one of the main uses of parantheses
+
 The u? part is always successful
 This isn't to say that any regex that contains ? is always successful
 
@@ -13,7 +16,8 @@ both 'colo' and 'u?' are successful, but r fails, so the whole regex fails
 
 echo ': "result.txt \n' > result.txt
 
-grep 'colou?r' -E sample.txt >> result.txt
+grep 'colou?r'  -E sample.txt >> result.txt
+grep '4(th)?$'   -E sample.txt >> result.txt
 
 cat 'sample.txt'
 cat 'result.txt'
