96010c52469f956923c9bf4534c4d959ae9b5c1b
minte9 <catalinprescure@gmail.com>
2022-12-05 19:17:52 +0200

    regex-pages update

--- main/grep/quantifiers/quantifiers.sh
+++ main/grep/quantifiers/quantifiers.sh
@@ -3,11 +3,16 @@
 : "Quantifiers
 Metacharacter + means one or more of the preceding item
 Metacharacter * means any (none or more), which is similar to the ?
+
+Notation {0,1} is the same as ?
+When single number is given, such as [a-z]{3}, 
+it matches exactly that many
 "
 
 echo ': "result.txt \n' > result.txt
 
 grep '<H[1-6] *>' -E sample.txt >> result.txt
+grep '<HR +SIZE *= *[0-9]+ *>' -E sample.txt >> result.txt
 
 cat 'sample.txt'
 cat 'result.txt'
