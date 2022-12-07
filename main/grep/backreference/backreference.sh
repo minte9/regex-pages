0c69fc34c6044de178c82efa560baa42028acbf6
minte9 <catalinprescure@gmail.com>
2022-12-07 09:55:59 +0200

    regex-pages update

--- main/grep/backreference/backreference.sh
+++ main/grep/backreference/backreference.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-: "Backreference
-Parentheses used to limit the scope of alternation 
-    (a|b)
-Parentheses group multiple characters to apply quantifiers 
-    (abc)?
-Parentheses can remember text matched by subexpression 
-    ([a-z]) \1
+: "Backreference with parantheses
+
+(a|b)       Parentheses used to limit the scope of alternation 
+(abc)?      Parentheses group multiple characters to apply quantifiers 
+([a-z]) \1  Parentheses can remember text matched by subexpression 
+    
 You can have more than one set of parentheses
 Use \1, \2, etc to refer to first, second, etc
 "
@@ -19,4 +18,5 @@ grep '\<(an) +\1\>' -E sample.txt >> result.txt
 cat sample.txt
 cat result.txt
 
+echo '---'
 grep '\<([a-z]+) \1\>' -E sample.txt --colour
