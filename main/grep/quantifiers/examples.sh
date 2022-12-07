3195f62fd6af4edbe3e2f27c27ccab9b65e3a2d1
minte9 <catalinprescure@gmail.com>
2022-12-07 13:17:16 +0200

    regex-pages update

--- main/grep/quantifiers/examples.sh
+++ main/grep/quantifiers/examples.sh
@@ -1,11 +1,7 @@
 : "Correct php variable names"
-
-grep '$[a-zA-Z_][a-zA-Z_0-9]*' examples.txt --colour
-
 : "Strings with double quotes"
-
-grep '"[^"]*"' -o examples.txt --colour
-
 : "Time of the day, such as 9:17 AM"
 
+grep '$[a-zA-Z_][a-zA-Z_0-9]*' examples.txt --colour
+grep '"[^"]*"' -o examples.txt --colour
 grep '[0-9]?[0-9]:[0-9][0-9] (am|pm)' -o -E examples.txt --colour
