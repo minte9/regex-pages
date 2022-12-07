0e75cba416186545363e0b2b2291e9ad05ed7bb0
minte9 <catalinprescure@gmail.com>
2022-12-07 13:18:08 +0200

    regex-pages update

--- main/grep/quantifiers/examples.sh
+++ main/grep/quantifiers/examples.sh
@@ -2,6 +2,6 @@
 : "Strings with double quotes"
 : "Time of the day, such as 9:17 AM"
 
-grep '$[a-zA-Z_][a-zA-Z_0-9]*' examples.txt --colour
-grep '"[^"]*"' -o examples.txt --colour
-grep '[0-9]?[0-9]:[0-9][0-9] (am|pm)' -o -E examples.txt --colour
+grep '$[a-zA-Z_][a-zA-Z_0-9]*'              examples.txt --colour
+grep '"[^"]*"'                           -o examples.txt --colour
+grep '[0-9]?[0-9]:[0-9][0-9] (am|pm)' -E -o examples.txt --colour
