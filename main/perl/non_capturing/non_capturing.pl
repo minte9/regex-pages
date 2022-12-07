4e2beada731acc5d26635d554255b471c5b6d184
minte9 <catalinprescure@gmail.com>
2022-12-07 16:33:56 +0200

    regex-pages update

---
+++ main/perl/non_capturing/non_capturing.pl
@@ -0,0 +1,19 @@
+#!/bin/perl -l
+
+=pod
+The surrounding ^$ ensures that the entire string contains only digits
+You can use the special notation (?: ), which group but do not capture
+This use of "?:" has no relation to the optional ? metacharacter
+=cut
+
+$s = "0728824242";
+if ($s =~ m/^[0-9]+$/) {
+    print "only digits";
+}
+
+$s = "-98.6 F";
+if($s =~ m/^([-+]?[0-9]+(\.[0-9]*)?) (?:[CF])$/) {
+    print $1; # -98.6
+    print $2; # .6
+    print $3; # nothing (not captured)
+}
