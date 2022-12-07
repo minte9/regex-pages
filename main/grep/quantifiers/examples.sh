1f1f6c45d5078241164498bf3780c86685b67661
minte9 <catalinprescure@gmail.com>
2022-12-07 13:04:35 +0200

    regex-pages update

---
+++ main/grep/quantifiers/examples.sh
@@ -0,0 +1,7 @@
+: "
+Search for correct php variable names
+Search strings with double quotes
+"
+
+grep '$[a-zA-Z_][a-zA-Z_0-9]*' examples.txt --colour
+grep '"[^"]*"' -o examples.txt --colour
--- main/grep/quantifiers/more_examples.sh
+++
@@ -1,7 +0,0 @@
-: "Search for correct php variable names"
-
-grep '$[a-zA-Z_][a-zA-Z_0-9]*' more_examples.txt --colour
-
-: "Search strings with double quotes"
-
-grep '"[^"]*"' -o more_examples.txt --colour
