5f880ca3a4b344f108f137c45ab77cb59cab35d0
minte9 <catalinprescure@gmail.com>
2022-12-07 13:00:30 +0200

    regex-pages update

--- main/grep/quantifiers/more_examples.sh
+++ main/grep/quantifiers/more_examples.sh
@@ -3,10 +3,8 @@ Search for correct php variable names
 Search strings with double quotes
 "
 
-echo ': "more_examples_result.txt \n' > more_examples_result.txt
-
-grep '$[a-zA-Z_][a-zA-Z_0-9]*' -o more_examples.txt >> more_examples_result.txt
-grep '"[^"]*"' -o more_examples.txt >> more_examples_result.txt
-
 cat 'more_examples.txt'
-cat 'more_examples_result.txt'
+echo '\n---'
+
+grep '$[a-zA-Z_][a-zA-Z_0-9]*' more_examples.txt --colour
+grep '"[^"]*"' -o more_examples.txt --colour
