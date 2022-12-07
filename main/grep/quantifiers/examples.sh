d7f9e1fb9a07c86b073b90e2cc1fc7017756f569
minte9 <catalinprescure@gmail.com>
2022-12-07 13:05:35 +0200

    regex-pages update

--- main/grep/quantifiers/examples.sh
+++ main/grep/quantifiers/examples.sh
@@ -1,7 +1,5 @@
-: "
-Search for correct php variable names
-Search strings with double quotes
-"
+: "Search for correct php variable names"
+: "Search strings with double quotes"
 
 grep '$[a-zA-Z_][a-zA-Z_0-9]*' examples.txt --colour
 grep '"[^"]*"' -o examples.txt --colour
