f9b0d72e1fd93f78b61c12f97bbd9e57df7430e1
minte9 <catalinprescure@gmail.com>
2022-12-08 18:26:40 +0200

    regex-pages update

--- main/perl/substitution/stock.pl
+++ main/perl/substitution/stock.pl
@@ -3,10 +3,9 @@
 Remove digits from stock proces, so
     12.37500000392 is reduce to 12.375, yet
     37.500 is reduced to 37.50
-    
 Shorthand \d matches a digit, 
-    [1-9]? removes a third posible zero (if any)
-    $1 capture first group in parantheses */
+Removes a third posible zero (if any) with [1-9]? 
+Group in parantheses to capture to $1 */
 =cut
 
 $a = '12.37500000392';
