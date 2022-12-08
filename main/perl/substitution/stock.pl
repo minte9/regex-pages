2eeed06dc425638fc2e6efedf9f7381ab855c82c
minte9 <catalinprescure@gmail.com>
2022-12-08 18:22:12 +0200

    regex-pages update

--- main/perl/substitution/stock.pl
+++ main/perl/substitution/stock.pl
@@ -1,11 +1,12 @@
 #!/bin/perl -l
 =pod /**
-We want to remove digits from stock proces, 
-so 12.37500000392 is reduce to 12.375, 
-yet 37.500 is reduced to 37.50
+Remove digits from stock proces, so
+12.37500000392 is reduce to 12.375, yt
+37.500 is reduced to 37.50
 
-Metacharacter \d matches a digit, 
-while [1-9]? removes a third posible zero (if any) */
+\d matches a digit, 
+[1-9]? removes a third posible zero (if any)
+$1 capture first group in parantheses */
 =cut
 
 $a = '12.37500000392';
