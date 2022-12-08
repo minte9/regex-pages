4453be5711191a76ef0a3d233968ed364b394a5e
minte9 <catalinprescure@gmail.com>
2022-12-08 15:09:45 +0200

    regex-pages update

--- main/perl/non_capturing/non_capturing.pl
+++ main/perl/non_capturing/non_capturing.pl
@@ -1,6 +1,6 @@
 #!/bin/perl -l
 
-=pod : "
+=pod "
 The surrounding ^$ ensures that the entire string contains only digits
 You can use the special notation (?: ), which group but do not capture
 This use of ?: has no relation to the optional ? metacharacter"
