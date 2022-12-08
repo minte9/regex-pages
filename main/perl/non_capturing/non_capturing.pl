ff6c3a2b52bef122588688c2c749ce8d304cb41c
minte9 <catalinprescure@gmail.com>
2022-12-08 15:14:35 +0200

    regex-pages update

--- main/perl/non_capturing/non_capturing.pl
+++ main/perl/non_capturing/non_capturing.pl
@@ -3,8 +3,8 @@
 =pod "
 The surrounding ^$ ensures that the entire string contains only digits
 You can use the special notation (?: ), which group but do not capture
-This use of ?: has no relation to the optional ? metacharacter"
-=cut
+This use of ?: has no relation to the optional ? metacharacter
+=cut "
 
 $s = "0728824242";
 if ($s =~ m/^[0-9]+$/) {
