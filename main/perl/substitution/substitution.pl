d96ee1ba295c66f9b11235c2bfe1d16301d82e56
minte9 <catalinprescure@gmail.com>
2022-12-09 23:13:14 +0200

    regex-pages update

--- main/perl/substitution/substitution.pl
+++ main/perl/substitution/substitution.pl
@@ -1,5 +1,6 @@
 #!/bin/perl -l
 =pod /**
+
 The text matched (if any) is replaced by replacement
 For word boundary use \b
 For global replacement use \g modifier */
@@ -13,6 +14,6 @@ $a =~ s/Jeff/Jeffrey/;
 $b =~ s/\bJeff\b/Jeffrey/; #// 
 $c =~ s/\bJeff\b/Jeffrey/g;
 
-print $a;   #// Jeffrey Friedl
-print $b;   #// Jeffrey Friedl (not Jeffreyrey Friedl)
-print $c;   #// Jeffrey is Jeffrey Friedl
+print $a;   # Jeffrey Friedl
+print $b;   # Jeffrey Friedl (not Jeffreyrey Friedl)
+print $c;   # Jeffrey is Jeffrey Friedl
