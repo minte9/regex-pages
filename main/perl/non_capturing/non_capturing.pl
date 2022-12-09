31c5e333180f3f9338a5f78dfa90183383916daf
minte9 <catalinprescure@gmail.com>
2022-12-09 23:17:18 +0200

    regex-pages update

--- main/perl/non_capturing/non_capturing.pl
+++ main/perl/non_capturing/non_capturing.pl
@@ -17,7 +17,7 @@ $s = '-98.6 F';
 $p = '^([-+]?[0-9]+(\.[0-9]*)?) (?:[CF])$';
 
 if( $s =~ m/$p/) {
-    print $1;   # -98.6
-    print $2;   # .6
-    print $3;   # not captured
+    print $1;   # '-98.6'
+    print $2;   # '.'6'
+    print $3;   # // null, not captured
 }
