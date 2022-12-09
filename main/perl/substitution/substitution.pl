62bec22ae58e4df889311285b0b47398646cff45
minte9 <catalinprescure@gmail.com>
2022-12-09 23:13:49 +0200

    regex-pages update

--- main/perl/substitution/substitution.pl
+++ main/perl/substitution/substitution.pl
@@ -11,9 +11,9 @@ $b = 'Jeffrey Friedl';
 $c = 'Jeff is Jeff Friedl';
 
 $a =~ s/Jeff/Jeffrey/;
-$b =~ s/\bJeff\b/Jeffrey/; #// 
+$b =~ s/\bJeff\b/Jeffrey/;
 $c =~ s/\bJeff\b/Jeffrey/g;
 
-print $a;   # Jeffrey Friedl
-print $b;   # Jeffrey Friedl (not Jeffreyrey Friedl)
-print $c;   # Jeffrey is Jeffrey Friedl
+print $a;   # // Jeffrey Friedl
+print $b;   # // Jeffrey Friedl (not Jeffreyrey Friedl)
+print $c;   # // Jeffrey is Jeffrey Friedl
