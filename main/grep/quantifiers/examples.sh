599bd303745b19f5222f3c29c497eac0f0dd6c80
minte9 <catalinprescure@gmail.com>
2022-12-09 22:30:27 +0200

    regex-pages update

--- main/grep/quantifiers/examples.sh
+++ main/grep/quantifiers/examples.sh
@@ -1,7 +1,20 @@
-: "Correct php variable names"
-: "Strings with double quotes"
-: "Time of the day, such as 9:17 AM"
+#!/bin/sh
 
-grep '$[a-zA-Z_][a-zA-Z_0-9]*'                    examples.txt --colour
-grep '"[^"]*"'                                 -o examples.txt --colour
-grep '^(1[012]|[1-9]):[0-5][0-9] (am|pm)'   -E -o examples.txt --colour
+: "Quantifiers:
+Correct php variable names
+Strings with double quotes
+Time of the day, such as 9:17 AM
+"
+
+A='$a, $_b, $C, but not $1a'
+B='"abc", but not "abc d'
+C='
+09:17 am
+99:99 pm
+9:17 am
+12:30 pm
+'
+
+echo $A | grep '\$[a-zA-Z_][a-zA-Z_0-9]*'            -o -E | tee result2.txt
+echo $B | grep '"[^"]*"'                            -o -E | tee result2.txt -a
+echo $C | grep '\<(1[012]|[1-9]):[0-5][0-9]\> (am|pm)' -o -E | tee result2.txt -a
--- main/grep/quantifiers/quantifiers.sh
+++ main/grep/quantifiers/quantifiers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-: "Quantifiers
+: "Quantifiers:
 Metacharacter + means one or more of the preceding item
 Metacharacter * means any (none or more), which is similar to the ?
 
@@ -9,10 +9,12 @@ When single number is given, such as [a-z]{3},
 it matches exactly that many
 "
 
-echo ': "result.txt \n' > result.txt
+A='<H1> <H2> <H3 > <H7>'
+B='
+<HR SIZE=14>
+<HR  SIZE = 14 >
+<HRSIZE=14>
+'
 
-grep '<H[1-6] *>'               -E sample.txt >> result.txt
-grep '<HR +SIZE *= *[0-9]+ *>'  -E sample.txt >> result.txt
-
-cat 'sample.txt'
-cat 'result.txt'
+echo $A | grep '<H[1-6] *>'               -o -E | tee result.txt
+echo $B | grep '<HR +SIZE *= *[0-9]+ *>'  -o -E | tee result.txt -a
