3eaa71ba73c3cada2feeb4e3e9a77dca4484d4fe
minte9 <catalinprescure@gmail.com>
2022-12-09 21:27:08 +0200

    regex-pages update

--- main/grep/classes/classes.sh
+++ main/grep/classes/classes.sh
@@ -13,5 +13,5 @@ Multiple ranges are fine [0-9a-z]
 A='gray grey greay greey gr-y'
 B='<H1></H1> <H22></H22> <H3></H3>'
 
-echo $A | grep 'gr[-ae]y' -o | tee    result.txt
-echo $B | grep '<H[1-3]>' -o | tee -a result.txt
+echo $A | grep 'gr[-ae]y' -o | tee result.txt
+echo $B | grep '<H[1-3]>' -o | tee result.txt -a
