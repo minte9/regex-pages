9c434d3303c998c55abbebb7fed4e7d922777bca
minte9 <catalinprescure@gmail.com>
2022-12-09 23:09:17 +0200

    regex-pages update

--- main/perl/non_capturing/non_capturing.pl
+++ main/perl/non_capturing/non_capturing.pl
@@ -1,5 +1,6 @@
 #!/bin/perl -l
 =pod /**
+
 The surrounding ^$ ensures that the entire string contains only digits
 You can use the special notation (?: ), which group but not capture
 This ?: has no relation to the optional ? metacharacter */
@@ -16,7 +17,7 @@ $s = '-98.6 F';
 $p = '^([-+]?[0-9]+(\.[0-9]*)?) (?:[CF])$';
 
 if( $s =~ m/$p/) {
-    print $1;   #// -98.6
-    print $2;   #// .6
-    print $3;   #// nothing (not captured)
+    print $1;   # -98.6
+    print $2;   # .6
+    print $3;   # nothing (not captured)
 }
