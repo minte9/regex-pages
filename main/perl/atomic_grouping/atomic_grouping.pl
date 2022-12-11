33ba4c9d92131c574a1b253035e78613cab011a6
minte9 <catalinprescure@gmail.com>
2022-12-11 16:23:48 +0200

    regex-pages update

---
+++ main/perl/atomic_grouping/atomic_grouping.pl
@@ -0,0 +1,17 @@
+#!/bin/perl -l
+=begin
+
+Atomic grouping (?> )
+
+The string ¡Hola! is match by ¡.*!, but is not match by atomic grouping ¡(?>.*)!
+In either case, .* first internaly matches as much as it can (¡Hola!)
+Then gives up some of what is match to force the regex to match (the final !)
+This doesn't happen when there is a atomic grouping
+
+=cut
+
+$a = "¡Hola!";
+$b = "¡Hola!";
+
+print ($a =~ m/¡(.*)!/);    # Hola
+print ($b =~ m/¡(?>.*)!/);  # Null // not matching
